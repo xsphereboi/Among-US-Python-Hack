833605c0cdd6e063d6dbc5e9d034fe0baff9f54a
Xsphere Torex <64603285+xsphereboi@users.noreply.github.com>
2020-10-23 14:24:19 +0545

    Added Imposter Access

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import pymem
 import pymem.process
-import time
-
+import psutil
+import os
 dwSpeed = 0x00DA3C30
-pm = pymem.Pymem("Among Us.exe")
+dwImposter = 0x00DA5A84
+
+pm = pymem.Pymem('Among Us.exe')
 
 def readBaseAddress():
     client = pymem.process.module_from_name(pm.process_handle,"GameAssembly.dll").lpBaseOfDll
@@ -23,10 +25,37 @@ def writeMemorySpeedx(val):
     new_speed = pm.write_float(intva,float(val))
     return 1
 
+def accessImposterFeatures():
+    client = pymem.process.module_from_name(pm.process_handle,"GameAssembly.dll").lpBaseOfDll
+    speed = pm.read_int(client+dwImposter)
+    off1 = pm.read_int(speed+0x5C)
+    off2 = pm.read_int(off1+0x0)
+    off3 = pm.read_int(off2+0x034)
+    off4 = hex(off3+0x28)
+    intoff4 = int(off4,16)
+    access = pm.write_int(intoff4,1)
+    return 'Acceesed'
 
+def intro():
+    print("""
+        ______                                  _   _ _____  
+    | ___ \                                | | | /  ___| 
+    | |_/ /   _ _ __ ___   ___  _ __   __ _| | | \ `--.  
+    |  __/ | | | '_ ` _ \ / _ \| '_ \ / _` | | | |`--. \ 
+    | |  | |_| | | | | | | (_) | | | | (_| | |_| /\__/ / 
+    \_|   \__, |_| |_| |_|\___/|_| |_|\__, |\___/\____/  
+        __/ |                       __/ |             
+        |___/                       |___/              
+        
+            Python Sample Code For Memory Reading/Writing\n\n""")
 if __name__ == "__main__":
-    print("Wellcome to AMONG US SPEED HACK BY XSPHERE : \n")
-    print("Current Speed Is :"+str(readAddressandSpeed()))
-    sx = input("Enter Your Desired Speed : ")
-    writeMemorySpeedx(sx)
-    print("New Speed Is :"+str(readAddressandSpeed()))
+    intro()
+    print("[1] Change Speed \n[2] Access Imposter Feature : ")
+    cheatOptions = int(input("Select your option either 1 or two : "))
+    if cheatOptions == 1:
+        speedtoset = int(input("        [+]Select Your Desired Speed : "))
+        writeMemorySpeedx(speedtoset)
+        print('        [+] Speed Changed!! Current Speed IS : '+str(readAddressandSpeed()))
+    elif cheatOptions == 2:
+        accessImposterFeatures()
+        print('        [+] You Now Have Imposter Access')
